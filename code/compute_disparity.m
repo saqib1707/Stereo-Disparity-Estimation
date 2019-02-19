9c172385bcea99ebe93e3b8fab8fa7389f2b7c9f
Saqib Azim <azimsaqib10@gmail.com>
2019-02-19 16:52:50 +0530

    the basic version is working. trying with edge images

--- code/compute_disparity.m
+++ code/compute_disparity.m
@@ -1,29 +1,32 @@
-function [disparity] = compute_disparity(left_img, right_img, window_size, max_search_space)
+function [disparity] = compute_disparity(left_img, right_img, window_size, max_search_space, option)
     progress_bar = waitbar(0,'Performing Pixel Intensity Matching...');
-    rows = size(left_img,1);
-    cols = size(left_img,2);
+    height = size(left_img,1);
+    width = size(left_img,2);
     feature_size = window_size.^2;
-    disparity = zeros(rows-window_size+1,cols-window_size+1);
-    left_descriptor = zeros(feature_size,cols-window_size+1);
-    right_descriptor = zeros(feature_size,cols-window_size+1);
-    for i=1:rows-window_size+1
-        for j=1:cols-window_size+1
+    disparity = zeros(height-window_size+1, width-window_size+1);
+    left_descriptor = zeros(feature_size, width-window_size+1);
+    right_descriptor = zeros(feature_size,width-window_size+1);
+    for i = 1:height-window_size+1
+        for j = 1:width-window_size+1
             left_descriptor(:,j) = reshape(left_img(i:i+window_size-1,j:j+window_size-1),feature_size,1);
             right_descriptor(:,j) = reshape(right_img(i:i+window_size-1,j:j+window_size-1),feature_size,1);
         end;
         
-        for j=1:cols-window_size+1
-            squared_error_list = [];
-            for k=j:min(j+max_search_space-1,cols-window_size+1)
-                squared_error = sum((left_descriptor(:,k) - right_descriptor(:,j)).^2);
-                squared_error_list = [squared_error_list, squared_error];
+        for j = 1:width-window_size+1
+            metric_list = [];
+            for k = j:min(j+max_search_space-1,width-window_size+1)
+                similarity_metric = compute_metric(left_descriptor(:,k), right_descriptor(:,j), option);
+                metric_list = [metric_list, similarity_metric];
             end
-            [~,loc] = min(squared_error_list);
+            if option == 1
+                [~,loc] = min(metric_list);
+            elseif option == 2
+                [~,loc] = max(metric_list);
+            end
             disparity(i,j) = loc;
         end
-        waitbar(i/rows, progress_bar);
+        waitbar(i/(height-window_size+1), progress_bar);
     end
     close(progress_bar);
     disparity = disparity/max(max(disparity));
 end
-
--- code/main.m
+++ code/main.m
@@ -1,10 +1,23 @@
 clc; clear;
-left_img = imread('../Stereo-Matching/teddy_left.png');
-right_img = imread('../Stereo-Matching/teddy_right.png');
-left_img = rgb2gray(left_img);
-right_img = rgb2gray(right_img);
-window_size = 3;
+left_img = imread('../dataset/barn1/im0.ppm');
+right_img = imread('../dataset/barn1/im8.ppm');
+left_disp_gt = imread('../dataset/barn1/disp2.pgm');
+right_disp_gt = imread('../dataset/barn1/disp6.pgm');
+left_edge_img = edge(rgb2gray(left_img),'canny',0.2);
+right_edge_img = edge(rgb2gray(right_img),'canny',0.2);
+
+figure;
+subplot(2,2,1);imshow(left_img);
+subplot(2,2,2);imshow(left_edge_img);
+subplot(2,2,3);imshow(right_img);
+subplot(2,2,4);imshow(right_edge_img);
+
+window_size = 5;
 max_search_space = 50;
-disparity = compute_disparity(left_img, right_img, window_size, max_search_space);
-imshow(left_img);
-imshow(disparity);
+option = 1;        % '1' -> euclidean distance based , '2' -> correlation based
+disparity_map = compute_disparity(left_edge_img, right_edge_img, window_size, max_search_space, option);
+figure;
+subplot(2,2,1);imshow(right_img);
+subplot(2,2,2);imshow(right_edge_img);
+subplot(2,2,3);imshow(right_disp_gt);
+subplot(2,2,4);imshow(disparity_map);
