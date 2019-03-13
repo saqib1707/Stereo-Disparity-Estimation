18758fca35002c5e03a5ace86e78d8c406227ba2
Saqib Azim <azimsaqib10@gmail.com>
2019-03-13 14:16:20 +0530

    update

--- code/disparity_constrained_optimization/compute_disparity_using_edge.m
+++ code/disparity_constrained_optimization/compute_disparity_using_edge.m
@@ -1,4 +1,4 @@
-function [disparity_map] = compute_disparity_edge(left_img, right_img, window_size, max_search_space, which_metric, canny_threshold, sigma)
+function [disparity_map] = compute_disparity_using_edge(left_img, right_img, window_size, max_search_space, which_metric, canny_threshold, sigma)
     progress_bar = waitbar(0,'Performing Stereo Correspondency Matching...');
     [height,width,~] = size(left_img);     % size([left image]) = size([right image])
     left_padded_img = zeros(height+window_size-1,width+window_size-1,3);
@@ -9,15 +9,14 @@ function [disparity_map] = compute_disparity_edge(left_img, right_img, window_si
     feature_size = 3*window_size^2;      % features taken across the 3-channels
     disparity = zeros(height,width);
 
-%     left_edge_img = edge(rgb2gray(left_img),'canny',canny_threshold);
     right_edge_img = edge(rgb2gray(right_img),'canny',canny_threshold);
     for row = 1:height
-        for col = 1:width
+        for col = 1:width-1
             if (right_edge_img(row,col) == 1)
-                metric_list = zeros(min(col+max_search_space-1,width)-col+1,1);
+                metric_list = zeros(min(col+max_search_space,width)-col,1);
                 count = 1;
                 right_descriptor = reshape(right_padded_img(row:row+window_size-1,col:col+window_size-1,:),feature_size,1);
-                for itr = col:min(col+max_search_space-1, width)
+                for itr = col+1:min(col+max_search_space, width)
                     left_descriptor = reshape(left_padded_img(row:row+window_size-1,itr:itr+window_size-1,:),feature_size,1);
                     similarity_metric = compute_metric(left_descriptor, right_descriptor, which_metric);
                     metric_list(count,1) = similarity_metric;
@@ -28,7 +27,7 @@ function [disparity_map] = compute_disparity_edge(left_img, right_img, window_si
                 elseif which_metric == 2
                     [~,loc] = max(metric_list);
                 end
-                XL = col + loc - 1;
+                XL = col + loc;
                 XR = col;
                 disparity(row,col) = XL - XR;
             end
@@ -37,9 +36,9 @@ function [disparity_map] = compute_disparity_edge(left_img, right_img, window_si
     end
     close(progress_bar);
     disparity = disparity/max(max(disparity));
-%     disparity_map = disparity;
+    disparity_map = disparity;
 
-    disparity_map = zeros(size(disparity));
+%     disparity_map = zeros(size(disparity));
 %     disparity_map = imgaussfilt(disparity, sigma);  %     smooth the disparity map using gaussian blur
     
 %     [y_nz, x_nz] = find(disparity_map ~= 0);
@@ -52,31 +51,31 @@ function [disparity_map] = compute_disparity_edge(left_img, right_img, window_si
 %     disparity_map(disparity_map == 0) = estimated_points;
 
     % optimization problem for smooth interpolation of disparity map
-    progress_bar = waitbar(0,'Performing optimization ...');
-    Aineq = [];
-    bineq = [];
-    Aeq = [];
-    beq = [];
-    patch_size = 20;
-    N = patch_size^2;
-    lower_bound(1:N,1) = 0;
-    upper_bound(1:N,1) = 1.0;
-
-    options = optimoptions('fmincon', 'Algorithm', 'sqp','MaxFunEvals', 200000, 'MaxIter', 1000, ...
-    'Display', 'iter', 'GradObj', 'off', 'DerivativeCheck','off', 'FinDiffType', 'central');
-
-    count = 0;
-    for row = 1:floor(height/patch_size)
-        for col = 1:floor(width/patch_size)
-            starting_point = reshape(im2double(rgb2gray(right_img(patch_size*(row-1)+1:patch_size*row,patch_size*(col-1)+1:patch_size*col,:))),N,1);
-            disparity_patch = disparity(patch_size*(row-1)+1:patch_size*row,patch_size*(col-1)+1:patch_size*col);
-            obj_func = @(x)objective_function(x, disparity_patch);
-            linearcons = @(x)constraints(x, disparity_patch);
-            [opt_x, fval, exitflag, output] = fmincon(obj_func, starting_point, Aineq, bineq, Aeq, beq, lower_bound, upper_bound, linearcons, options);
-            disparity_map(patch_size*(row-1)+1:patch_size*row,patch_size*(col-1)+1:patch_size*col) = reshape(opt_x,patch_size,patch_size);
-            count = count + 1;
-            waitbar(count/(floor(height/patch_size)*floor(width/patch_size)), progress_bar);
-        end
-    end
-    close(progress_bar);
+%     progress_bar = waitbar(0,'Performing optimization ...');
+%     Aineq = [];
+%     bineq = [];
+%     Aeq = [];
+%     beq = [];
+%     patch_size = 20;
+%     N = patch_size^2;
+%     lower_bound(1:N,1) = 0;
+%     upper_bound(1:N,1) = 1.0;
+% 
+%     options = optimoptions('fmincon', 'Algorithm', 'sqp','MaxFunEvals', 200000, 'MaxIter', 1000, ...
+%     'Display', 'iter', 'GradObj', 'off', 'DerivativeCheck','off', 'FinDiffType', 'central');
+% 
+%     count = 0;
+%     for row = 1:floor(height/patch_size)
+%         for col = 1:floor(width/patch_size)
+%             starting_point = reshape(im2double(rgb2gray(right_img(patch_size*(row-1)+1:patch_size*row,patch_size*(col-1)+1:patch_size*col,:))),N,1);
+%             disparity_patch = disparity(patch_size*(row-1)+1:patch_size*row,patch_size*(col-1)+1:patch_size*col);
+%             obj_func = @(x)objective_function(x, disparity_patch);
+%             linearcons = @(x)constraints(x, disparity_patch);
+%             [opt_x, fval, exitflag, output] = fmincon(obj_func, starting_point, Aineq, bineq, Aeq, beq, lower_bound, upper_bound, linearcons, options);
+%             disparity_map(patch_size*(row-1)+1:patch_size*row,patch_size*(col-1)+1:patch_size*col) = reshape(opt_x,patch_size,patch_size);
+%             count = count + 1;
+%             waitbar(count/(floor(height/patch_size)*floor(width/patch_size)), progress_bar);
+%         end
+%     end
+%     close(progress_bar);
 end
--- code/disparity_using_matlab_inbuilt/matlab_stereo.m
+++ code/disparity_using_matlab_inbuilt/matlab_stereo.m
@@ -1,8 +1,8 @@
 clc; clear;
 addpath(genpath('../'));
 root_folder = '../../dataset/mypics/';
-left_img_file = 'im2.jpg';
-right_img_file = 'im3.jpg';
+left_img_file = 'im4.jpg';
+right_img_file = 'im5.jpg';
 left_disp_file = 'disp2.pgm';
 right_disp_file = 'disp6.pgm';
 
@@ -11,8 +11,8 @@ right_img = imread(strcat(root_folder,right_img_file));
 left_disp_gt = imread(strcat(root_folder,left_disp_file));
 right_disp_gt = imread(strcat(root_folder,right_disp_file));
 
-scale = 0.1;
-[left_img,right_img,~,~] = myresize(left_img,right_img,left_disp_gt,right_disp_gt,scale);
+% scale = 0.1;
+% [left_img,right_img,~,~] = myresize(left_img,right_img,left_disp_gt,right_disp_gt,scale);
 
 left_gray  = rgb2gray(left_img);
 right_gray = rgb2gray(right_img);
@@ -20,5 +20,6 @@ right_gray = rgb2gray(right_img);
 disparity_map = im2double(disparity(left_gray, right_gray));
 disparity_map = disparity_map/max(max(disparity_map));
 figure;
-subplot(1,2,1);imshow(right_img);title('right image');
-subplot(1,2,2);imshow(disparity_map);title('disparity map (in-built)');
+subplot(2,2,1);imshow(left_img);title('left image');
+subplot(2,2,3);imshow(right_img);title('right image');
+subplot(2,2,4);imshow(disparity_map);title('computed disparity (in-built)');
