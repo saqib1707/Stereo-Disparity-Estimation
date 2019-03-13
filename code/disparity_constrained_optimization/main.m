07cff95b75b4b648eba9ee7f4bf7a493bbd992c6
Saqib Azim <azimsaqib10@gmail.com>
2019-03-13 19:18:17 +0530

    restructured code and added some more results after final demo evaluation

--- code/disparity_constrained_optimization/main.m
+++ code/disparity_constrained_optimization/main.m
@@ -1,15 +1,15 @@
 clc; clear;
 addpath(genpath('../'));
-root_folder = '../../dataset/mypics/';
-left_img_file = 'im8.jpg';
-right_img_file = 'im9.jpg';
+root_folder = '../../dataset/barn1/';
+left_img_file = 'im2.ppm';
+right_img_file = 'im6.ppm';
 left_disp_file = 'disp2.pgm';
 right_disp_file = 'disp6.pgm';
 
 % hyper-parameters
-window_size = 15;
+window_size = 9;
 assert(mod(window_size,2) == 1);
-max_search_space = 50;
+max_search_space = 30;
 which_metric = 1;        % '1' -> euclidean distance based , '2' -> correlation based
 canny_threshold = 0.1;
 sigma = 0.1;
@@ -26,13 +26,20 @@ right_disp_gt = imread(strcat(root_folder,right_disp_file));
 disparity_map = compute_disparity_full(left_img, right_img, window_size, max_search_space, which_metric, sigma);
 % disparity_map = compute_disparity_using_edge(left_img, right_img, window_size, max_search_space, which_metric, canny_threshold, sigma);
 
+% disparity using matlab inbuilt function
+% left_gray  = rgb2gray(left_img);
+% right_gray = rgb2gray(right_img);
+% 
+% disparity_inbuilt = im2double(disparity(left_gray, right_gray));
+% disparity_inbuilt = disparity_inbuilt/max(max(disparity_inbuilt));
+
 fig1 = figure;
 subplot(2,2,1);imshow(left_img);title('left image');
-subplot(2,2,2);imshow(left_edge_img);title('left edge image');
+subplot(2,2,2);imshow(right_disp_gt);title('disparity ground truth');
 subplot(2,2,3);imshow(right_img);title('right image');
 subplot(2,2,4);imshow(disparity_map);title('disparity computed');
-print(fig1,'-dpng','-r0','../../results/disparity-results/mypics/im89_ws_15_mss_50_1.png');
+print(fig1,'-dpng','-r0','../../results/disparity-results-full/barn1/im26_ws_9_mss_30_1.png');
 fig2 = figure;
-% subplot(1,2,1);imshow(right_disp_gt);title('disparity ground truth');
-subplot(1,1,1);imshow(disparity_map);title('disparity computed');
-print(fig2,'-dpng','-r0','../../results/disparity-results/mypics/im89_ws_15_mss_50_2.png')
+subplot(1,2,1);imshow(right_disp_gt);title('disparity ground truth');
+subplot(1,2,2);imshow(disparity_map);title('disparity computed');
+print(fig2,'-dpng','-r0','../../results/disparity-results-full/barn1/im26_ws_9_mss_30_2.png')
--- code/disparity_using_matlab_inbuilt/matlab_stereo.m
+++ code/disparity_using_matlab_inbuilt/matlab_stereo.m
@@ -1,8 +1,8 @@
 clc; clear;
 addpath(genpath('../'));
-root_folder = '../../dataset/mypics/';
-left_img_file = 'im4.jpg';
-right_img_file = 'im5.jpg';
+root_folder = '../../dataset/venus/';
+left_img_file = 'im2.ppm';
+right_img_file = 'im6.ppm';
 left_disp_file = 'disp2.pgm';
 right_disp_file = 'disp6.pgm';
 
@@ -10,9 +10,6 @@ left_img = imread(strcat(root_folder,left_img_file));
 right_img = imread(strcat(root_folder,right_img_file));
 left_disp_gt = imread(strcat(root_folder,left_disp_file));
 right_disp_gt = imread(strcat(root_folder,right_disp_file));
-
-% scale = 0.1;
-% [left_img,right_img,~,~] = myresize(left_img,right_img,left_disp_gt,right_disp_gt,scale);
 
 left_gray  = rgb2gray(left_img);
 right_gray = rgb2gray(right_img);
@@ -22,4 +19,5 @@ disparity_map = disparity_map/max(max(disparity_map));
 figure;
 subplot(2,2,1);imshow(left_img);title('left image');
 subplot(2,2,3);imshow(right_img);title('right image');
+subplot(2,2,2);imshow(left_disp_gt);title('disparity ground truth');
 subplot(2,2,4);imshow(disparity_map);title('computed disparity (in-built)');
